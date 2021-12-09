afcef18de3a122ef4cfcd4d3112b4f6d73038272
zechlau14 <73336747+zechlau14@users.noreply.github.com>
2021-12-09 11:56:11 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,11 @@ let h = 3; // room height (m)
 let x_o = l/2; //x-coordinate of infectious person (m)
 let y_o = w/2; //y-coordinate of infectious person (m)
 let R = 0.5; //infectious particle emission rate (particles/s)
-let v_x=0.1; //airflow velocity assumed in x-direction (m/s)
+let v_x=0.15; //airflow velocity assumed in x-direction (m/s)
 let v_y=0; //airflow velocity assumed in y-direction (m/s)
 let d = 1.7*10**(-4); //virus deactivation rate (s^-1)
 let s = 1.1*10**(-4); // particle settling rate (s^-1)
-let Q = 0.5; // air exchange rate (h^-1)
+let Q = 0.72; // air exchange rate (h^-1)
 Q = Q/3600; // convert Q from h^{-1} to s^{-1}
 let mask_ex = 1; // mask exhalation efficiency of infectious person
 let mask_in = 1; // mask inhalation efficiency of susceptible person
@@ -23,8 +23,8 @@ let x = x_o+2; // x-coordinate of susceptible person 1 (m)
 let y = y_o; // y-coordinate of susceptible person 1 (m)
 let x2 = l; // x-coordinate of susceptible person 2 (m)
 let y2 = w; // y-coordinate of susceptible person 2 (m)
-let delta_x = 2; // size of x-step (m)
-let delta_y = 2; // size of y-step (m)
+let delta_x = 0.1; // size of x-step (m)
+let delta_y = 0.1; // size of y-step (m)
 
 //Define time-axis from input given
 //Key functions, as calling the result of t_array is required for each of the other functions
