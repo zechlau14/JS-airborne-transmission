33cb4590f80508b0dad25ec89c274a97be8353c4
zechlau14 <73336747+zechlau14@users.noreply.github.com>
2022-04-01 16:13:13 +0800

    Add files via upload

--- webapp.js
+++ webapp.js
@@ -1,9 +1,10 @@
 function graph_choice(){
-    //Get the checkbox
+    //check result of toggle switch "contour-or-point"
+    //if box is checked show inputs for coordinates; else hide
+
     var checkBox = document.getElementById("contour-or-point");
     var coord_input = document.getElementById("coord-eval");
 
-    //show the correct button
     if(checkBox.checked == false){
         coord_input.style.display='none';
     } else{
@@ -12,12 +13,12 @@ function graph_choice(){
 }
 
 function model_choice(){
-    //Get the checkbox
+    //check the toggle switch for risk or concentration
+    //show the button that runs the correct function.
     var checkBox = document.getElementById("risk-or-conc");
     var risk_button = document.getElementById("run-risk");
     var conc_button = document.getElementById("run-conc");
 
-    //show the correct button
     if(checkBox.checked == false){
         conc_button.style.display='none';
         risk_button.style.display='block';
@@ -28,6 +29,8 @@ function model_choice(){
 }
 
 function add_break(){
+    //check the checkbox for adding a break
+    //if checked, show the inputs for a break.
     var checkBox = document.getElementById("break");
     var break_input = document.getElementById("break-time");
     var break_when = document.getElementById("break-when");
@@ -42,6 +45,8 @@ function add_break(){
 }
 
 function advanced_options(){
+    //function for advanced options button
+    //when button is pressed, show the advanced-options inputs
     var checkBox = document.getElementById("advanced-options");
     if(checkBox.style.display == "none"){
         checkBox.style.display = "block";
@@ -57,6 +62,7 @@ const s = 3.397*10**(-5);
 const delta_t = 1;
 
 function run_risk(){
+    //erase the results from previous runs of the model
     const chart = document.getElementById("result");
     chart.innerHTML="";
     const avg_print = document.getElementById("avg");
@@ -64,11 +70,12 @@ function run_risk(){
     const inf_print = document.getElementById("infected");
     inf_print.innerHTML="";
 
+    //Show the "loading" image in the output section.
     const output = document.getElementById("output");
     output.style.display='block';
     $("#loading").show();
     setTimeout(function(){
-
+    //reads all the inputs from the page.
     var l = document.getElementById("room-length").value;
     l = parseFloat(l);    
     var w = document.getElementById("room-width").value;
@@ -141,12 +148,16 @@ function run_risk(){
     var breakBox = document.getElementById("break");
 
     if(checkBox.checked == false){
+        //if toggle button for whole room is chosen
+        //calculate x and y coordinates for the graph.
         var xx = x_array(l,delta_x);
         var yy = y_array(w,delta_y);
 
         if(breakBox.checked == false){
+            //if no break is selected, run P_contour function
             var zz = P_contour(time,R,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,p,r,mask_ex,mask_in,frac_speak,delta_t,delta_x,delta_y);
         } else{
+            //if a break is selected, obtain those parameters, then run P_break_contour function
             var time_break = document.getElementById("break-duration").value;
             time_break = parseFloat(time_break);
             var time2 = document.getElementById("break-start").value;
@@ -155,6 +166,7 @@ function run_risk(){
             var zz = P_break_contour(time_break,time_cont,time2,R,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,p,r,mask_ex,mask_in,frac_speak,delta_t,delta_x,delta_y);
         }
 
+        //Plot results
         var datapt = [{
             x: xx,
             y: yy,
@@ -163,21 +175,25 @@ function run_risk(){
         }];
         Plotly.newPlot(chart,datapt,{margin:{t:0}});
         
+        //run avg function then print average probability
+        //and translate it to a figure given the number of occupants
         var P_avg = avg(zz,l,w,delta_x,delta_y);
-        console.log(typeof P_avg);
         P_avg = P_avg*100;
         avg_print.innerHTML = "<p> Average infection risk from airborne transmission in the room is " + P_avg + "%. </p>";
         inf_print.innerHTML = "<p> ie. <output>" + P_avg + "</output> out of <input type='number' value='100' min='0' step='1' style='width:45px' oninput='this.previousElementSibling.value = this.value/100*" + P_avg+"'> </input> people in the room will likely be infected.</p>";
     } else{
+        //toggle button chooses evaluate at a specific location, so read the coordinates of the location
         var x_e = document.getElementById("x_e").value;
         x_e = parseFloat(x_e);
         var y_e = document.getElementById("y_e").value;
         y_e = parseFloat(y_e);
 
         if(breakBox.checked == false){
+            //if no break is chosen, run Prob function.
             var t_chart = t_graph(time,delta_t);
             var zz = Prob(R,time,x_e,y_e,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t,p,r,mask_in);
         } else{
+            //if add_break is chosen, get break parameters and run Prob_break function.
             var time_break = document.getElementById("break-duration").value;
             time_break = parseFloat(time_break);
             var time2 = document.getElementById("break-start").value;
@@ -187,6 +203,7 @@ function run_risk(){
             var zz = Prob_break(time_break,time_cont,R,time2,x_e,y_e,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t,p,r,mask_in);
         }
         
+        //plot graph
         var datapt = [{
             x: t_chart,
             y: zz
@@ -200,6 +217,7 @@ function run_risk(){
 }
 
 function run_conc(){
+    //erase the results from previous runs of the model
     const chart = document.getElementById("result");
     chart.innerHTML="";
     const avg_print = document.getElementById("avg");
@@ -207,12 +225,13 @@ function run_conc(){
     const inf_print = document.getElementById("infected");
     inf_print.innerHTML="";
 
+    //show loading screen in output div
     const output = document.getElementById("output");
     output.style.display='block';
     $("#loading").show();
 
     setTimeout(function(){
-
+    //read parameter inputs
     var l = document.getElementById("room-length").value;
     l = parseFloat(l);    
     var w = document.getElementById("room-width").value;
@@ -285,12 +304,16 @@ function run_conc(){
     var breakBox = document.getElementById("break");
 
     if(checkBox.checked == false){
+    //if the toggle switch chooses the whole room
+    //calc x and y values for graph.
     var xx = x_array(l,delta_x);
     var yy = y_array(w,delta_y);
 
     if(breakBox.checked == false){
+        //if no break is added, run C_contour function
         var zz = C_contour(time,R,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t,delta_x,delta_y);
     }else{
+        //if a break is added, get break parameters and run C_break_contour function
         var time_break = document.getElementById("break-duration").value;
         time_break = parseFloat(time_break);
         var time2 = document.getElementById("break-start").value;
@@ -298,6 +321,7 @@ function run_conc(){
         var time_cont = time - time2;
         var zz = C_break_contour(time_break,time_cont,time2,R,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t,delta_x,delta_y);
     }
+    //plot contour graph
     var datapt = [{
         x: xx,
         y: yy,
@@ -306,18 +330,23 @@ function run_conc(){
     }];
     Plotly.newPlot(chart,datapt,{margin:{t:0}});
 
+    //calc avg concentration and print result.
     var C_avg = avg(zz,l,w,delta_x,delta_y);
     avg_print.innerHTML = "<p> Average concentration in the room is " + C_avg + " particles/m<sup>3</sup>. </p>";
     }else{
+        //if specific location is chosen
+        //read x and y coordinate of specific location
         var x_e = document.getElementById("x_e").value;
         x_e = parseFloat(x_e);
         var y_e = document.getElementById("y_e").value;
         y_e = parseFloat(y_e);
 
         if(breakBox.checked == false){
+            //if no break is added, run C function
             var t_chart = t_graph(time,delta_t);
             var zz = C(R,time,x_e,y_e,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t,p,r,mask_in);
         }else{
+            //if break is added, get break parameters and run C_break function.
             var time_break = document.getElementById("break-duration").value;
             time_break = parseFloat(time_break);
             var time2 = document.getElementById("break-start").value;
@@ -327,6 +356,7 @@ function run_conc(){
             var t_chart = t_graph(time+time_break,delta_t);
             var zz = C_break(time_break,time_cont,R,time2,x_e,y_e,x_o,y_o,l,w,v_x,v_y,Q,d,s,h,mask_ex,frac_speak,delta_t);
         }
+        //plot line graph.
         var datapt = [{
             x: t_chart,
             y: zz
@@ -338,6 +368,7 @@ function run_conc(){
 }
 
 function toggleField(hideObj,showObj){
+    //function to toggle custom value input
     hideObj.disabled=true;        
     hideObj.style.display='none';
     showObj.disabled=false;   
@@ -346,6 +377,7 @@ function toggleField(hideObj,showObj){
   }
 
 function pass_break(){
+    //function to pass duration value selected by user to the break slider
     var duration = document.getElementById("duration").value;
     var breaky = document.getElementById("break-start");
 
@@ -353,6 +385,7 @@ function pass_break(){
 }
 
 function pass_x_coord(){
+    //function to pass room length selected by user to the x coordinate sliders
     var l = document.getElementById("room-length").value;
     var x = document.getElementById("x0");
     var x_e = document.getElementById("x_e");
@@ -366,6 +399,7 @@ function pass_x_coord(){
 }
 
 function pass_y_coord(){
+    //function to pass room width selected by user to the y coordinate sliders
     var w = document.getElementById("room-width").value;
     var y = document.getElementById("y0");
     var y_e = document.getElementById("y_e");
